9fb7c1a8490f85c2dcba7f8757b6b7525fa3461f
Habib <hrekns@gmail.com>
2017-11-01 14:45:52 +0600

    added inline css but need to do the designing well without inline css....

--- html5VideoPlayer.php
+++ html5VideoPlayer.php
@@ -13,45 +13,47 @@
 <html>
     <head>
         <title>Habib Video Player</title>
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" >
+        <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" >
 
         <style>
 
-            <link rel="stylesheet" type="text/css" href="css/main-style.css" >
 
-            div#videoControlBar
-            {
-                background: #333;
-                padding: 10px;
-            }
 
-            div#videoPlayerBox
-            {
-                background: #000;
-                width: 40%;
-            }
+            /*div#videoControlBar*/
+            /*{*/
+                /*background: #333;*/
+                /*padding: 10px;*/
+            /*}*/
 
-            #curentTimeText
-            {
-                float: right;
-                color: azure;
-            }
+            /*div#videoPlayerBox*/
+            /*{*/
+                /*background: #000;*/
+                /*width: 40%;*/
+            /*}*/
 
-            #durentTimeText
-            {
-                float: right;
-                color: azure;
-            }
+            /*#curentTimeText*/
+            /*{*/
+                /*float: right;*/
+                /*color: azure;*/
+            /*}*/
 
-            #btwDandC
-            {
-                float: right;
-                color: azure;
-            }
+            /*#durentTimeText*/
+            /*{*/
+                /*float: right;*/
+                /*color: azure;*/
+            /*}*/
 
-            #muteBtn
-            {
-                float: right;
-            }
+            /*#btwDandC*/
+            /*{*/
+                /*float: right;*/
+                /*color: azure;*/
+            /*}*/
+
+            /*#muteBtn*/
+            /*{*/
+                /*float: right;*/
+            /*}*/
 
             video::-webkit-media-controls-enclosure
             {
@@ -65,21 +67,65 @@
     </head>
 
     <body>
-        <div id="videoPlayerBox">
-            <video style="padding: 8px;" id="myVideo" autoplay width="95%" height="200px">
-                <source src="animation.mp4">
-                <source src="animation.ogg">
-            </video>
-            <div id="videoControlBar">
-                <input id="seekSlider" type="range" min="0" max="100" value="0" step="1" style="width: 100%;" />
-                <button id="playPauseButton">pause</button>
-                <span id="curentTimeText"></span> <span id="btwDandC">/</span> <span id="durentTimeText"></span><br>
-                <button id="muteBtn">Mute</button>
-                <input id="volumeSlider" type="range" min="0" max="100" value="100" step="1" style="width: 30%;" />
-                <button id="fullScreen">[ &nbsp; ]</button>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div>
+                        <video id="myVideo" autoplay width="100%" height="200px">
+                            <source src="animation.mp4">
+                            <source src="animation.ogg">
+                        </video>
+                    </div>
+
+                    <div align="center" style="background: rgba(43, 28, 11, 0.4); border: 1px solid #fdf7f7; height: 51px; padding: 2px;">
+                        <span style="font-size: 10px; float: right !important;" id="curentTimeText"></span> <span style="font-size: 10px; float: right !important;" id="btwDandC">/</span> <span style="font-size: 10px; float: right !important;" id="durentTimeText"></span>
+                        <span id="playPauseButton"><i style=" font-size: .7em; float: left !important;" class="fa fa-pause" aria-hidden="true"></i></span>
+                        <span style="float: right !important; margin-right: 2%; hight:2%;" id="fullScreen"><i style=" font-size: .6em;" class="fa fa-arrows-alt" aria-hidden="true"></i></span>
+                        <br/>
+
+                        <input id="seekSlider" type="range" min="0" max="100" value="0" step="1" style="width: 60%; float: left !important;" />
+
+                        <span>
+<!--                            <span style="float: right !important; margin-left: 2%;" id="fullScreen"><i style=" font-size: .7em;" class="fa fa-arrows-alt" aria-hidden="true"></i></span>-->
+                            <input id="volumeSlider" type="range" min="0" max="100" value="50" step="1" style="width: 20%;float: right !important;" />
+                            <span style="float: right !important; margin-right: 2%;" id="muteBtn"><i style=" font-size: .7em;" class="fa fa-volume-up" aria-hidden="true"></i></span>
+                        </span>
+                    </div>
+                </div>
+
 
             </div>
+
         </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--        <div id="videoPlayerBox">-->
+<!--            <video style="padding: 8px;" id="myVideo" autoplay width="95%" height="200px">-->
+<!--                <source src="animation.mp4">-->
+<!--                <source src="animation.ogg">-->
+<!--            </video>-->
+<!--            <div id="videoControlBar">-->
+<!--                <input id="seekSlider" type="range" min="0" max="100" value="0" step="1" style="width: 100%;" />-->
+<!--                <button id="playPauseButton">pause</button>-->
+<!--                <span id="curentTimeText"></span> <span id="btwDandC">/</span> <span id="durentTimeText"></span><br>-->
+<!--                <button id="muteBtn">Mute</button>-->
+<!--                <input id="volumeSlider" type="range" min="0" max="100" value="100" step="1" style="width: 30%;" />-->
+<!--                <button id="fullScreen">[ &nbsp; ]</button>-->
+<!---->
+<!--            </div>-->
+<!--        </div>-->
 
 
 
@@ -118,12 +164,12 @@
                 if(vid.paused)
                 {
                     vid.play();
-                    playBtn.innerHTML = 'pause';
+                    playBtn.innerHTML = '<i style="font-size: .7em; float: left !important;" class="fa fa-pause" aria-hidden="true"></i>';
                 }
                 else
                 {
                     vid.pause();
-                    playBtn.innerHTML = 'play';
+                    playBtn.innerHTML = '<i style="font-size: .7em; float: left !important;" class="fa fa-play" aria-hidden="true"></i>';
                 }
                 
             }
@@ -156,13 +202,13 @@
                 if(vid.muted)
                 {
                     vid.muted = false;
-                    muteBtn.innerHTML = 'Mute';
+                    muteBtn.innerHTML = '<i style="font-size: .7em;" class="fa fa-volume-up" aria-hidden="true"></i>';
                     volumeSlider.value = 100;
                 }
                 else
                 {
                     vid.muted = true;
-                    muteBtn.innerHTML = 'Unmute';
+                    muteBtn.innerHTML = '<i style="font-size: .7em; " class="fa fa-volume-off" aria-hidden="true"></i>';
                     volumeSlider.value = 0;
                 }
             }
