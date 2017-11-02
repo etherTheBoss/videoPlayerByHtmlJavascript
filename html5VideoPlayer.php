5fc855edf66a539f67fef88ec387a8eb25cec5b8
Habib <hrekns@gmail.com>
2017-11-02 14:04:51 +0600

    final without disigning...

--- html5VideoPlayer.php
+++ html5VideoPlayer.php
@@ -3,10 +3,10 @@
  * Created by
  * HABIBUR RAHMAN.
  * Sr.Software Engineer
+ * hrekns@yahoo.com
  * User: Habib
  * Date: 10/18/2017
  * Time: 5:02 PM
- * SOURCE : YOUTUBE https://www.youtube.com/watch?v=BuaIDNu_pPg
  */
 ?>
 <!DOCTYPE html>
@@ -17,43 +17,6 @@
         <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" >
 
         <style>
-
-
-
-            /*div#videoControlBar*/
-            /*{*/
-                /*background: #333;*/
-                /*padding: 10px;*/
-            /*}*/
-
-            /*div#videoPlayerBox*/
-            /*{*/
-                /*background: #000;*/
-                /*width: 40%;*/
-            /*}*/
-
-            /*#curentTimeText*/
-            /*{*/
-                /*float: right;*/
-                /*color: azure;*/
-            /*}*/
-
-            /*#durentTimeText*/
-            /*{*/
-                /*float: right;*/
-                /*color: azure;*/
-            /*}*/
-
-            /*#btwDandC*/
-            /*{*/
-                /*float: right;*/
-                /*color: azure;*/
-            /*}*/
-
-            /*#muteBtn*/
-            /*{*/
-                /*float: right;*/
-            /*}*/
 
             video::-webkit-media-controls-enclosure
             {
@@ -67,65 +30,35 @@
     </head>
 
     <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <div>
-                        <video id="myVideo" autoplay width="100%" height="200px">
-                            <source src="animation.mp4">
-                            <source src="animation.ogg">
-                        </video>
-                    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <div>
+                    <video id="myVideo" autoplay width="100%" height="200px">
+                        <source src="animation.mp4">
+                        <source src="animation.ogg">
+                    </video>
+                </div>
 
-                    <div align="center" style="background: rgba(43, 28, 11, 0.4); border: 1px solid #fdf7f7; height: 51px; padding: 2px;">
-                        <span style="font-size: 10px; float: right !important;" id="curentTimeText"></span> <span style="font-size: 10px; float: right !important;" id="btwDandC">/</span> <span style="font-size: 10px; float: right !important;" id="durentTimeText"></span>
-                        <span id="playPauseButton"><i style=" font-size: .7em; float: left !important; margin-left: 0.5% !important;" class="fa fa-pause" aria-hidden="true"></i></span>
-                        <span style="float: right !important; margin-right: 2%; hight:2%;" id="fullScreen"><i style=" font-size: .6em;" class="fa fa-arrows-alt" aria-hidden="true"></i></span>
-                        <br/>
+                <div align="center" style="background: rgba(43, 28, 11, 0.4); border: 1px solid #fdf7f7; height: 51px; padding: 2px;">
+                    <span style="font-size: 10px; float: right !important;" id="curentTimeText"></span> <span style="font-size: 10px; float: right !important;" id="btwDandC">/</span> <span style="font-size: 10px; float: right !important;" id="durentTimeText"></span>
+                    <span id="playPauseButton"><i style=" font-size: .7em; float: left !important; margin-left: 0.5% !important;" class="fa fa-pause" aria-hidden="true"></i></span>
+                    <span style="float: right !important; margin-right: 2%;" id="fullScreen"><i style=" font-size: .6em; vertical-align: top;" class="fa fa-arrows-alt" aria-hidden="true"></i></span>
+                    <br/>
 
-                        <input id="seekSlider" type="range" min="0" max="100" value="0" step="1" style="width: 60%; float: left !important;" />
+                    <input id="seekSlider" type="range" min="0" max="100" value="0" step="1" style="width: 60%; float: left !important;" />
 
-                        <span>
-<!--                            <span style="float: right !important; margin-left: 2%;" id="fullScreen"><i style=" font-size: .7em;" class="fa fa-arrows-alt" aria-hidden="true"></i></span>-->
+                    <span>
                             <input id="volumeSlider" type="range" min="0" max="100" value="50" step="1" style="width: 20%;float: right !important;" />
                             <span style="float: right !important; margin-right: 2%;" id="muteBtn"><i style=" font-size: .7em;" class="fa fa-volume-up" aria-hidden="true"></i></span>
                         </span>
-                    </div>
                 </div>
-
-
             </div>
+
 
         </div>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!--        <div id="videoPlayerBox">-->
-<!--            <video style="padding: 8px;" id="myVideo" autoplay width="95%" height="200px">-->
-<!--                <source src="animation.mp4">-->
-<!--                <source src="animation.ogg">-->
-<!--            </video>-->
-<!--            <div id="videoControlBar">-->
-<!--                <input id="seekSlider" type="range" min="0" max="100" value="0" step="1" style="width: 100%;" />-->
-<!--                <button id="playPauseButton">pause</button>-->
-<!--                <span id="curentTimeText"></span> <span id="btwDandC">/</span> <span id="durentTimeText"></span><br>-->
-<!--                <button id="muteBtn">Mute</button>-->
-<!--                <input id="volumeSlider" type="range" min="0" max="100" value="100" step="1" style="width: 30%;" />-->
-<!--                <button id="fullScreen">[ &nbsp; ]</button>-->
-<!---->
-<!--            </div>-->
-<!--        </div>-->
+    </div>
 
 
 
